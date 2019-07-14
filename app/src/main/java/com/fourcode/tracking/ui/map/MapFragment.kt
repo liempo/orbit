9d2816516336f29693f33601fb950cf98157f073
53n531 <gonzalesalec@gmail.com>
2019-07-15 01:05:04 +0800

    Template code for single activity style

---
+++ app/src/main/java/com/fourcode/tracking/ui/map/MapFragment.kt
@@ -0,0 +1,27 @@
+package com.fourcode.tracking.ui.map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fourcode.tracking.R
+
+class MapFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MapFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.map_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
