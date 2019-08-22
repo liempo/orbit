035a442c0351528549364374ab010853646ec1a9
Dragonborn <gonzalesalec@gmail.com>
2019-08-22 11:27:10 +0800

    Planted debug tree in Application level

---
+++ app/src/main/java/com/fourcode/tracking/TimberApplication.kt
@@ -0,0 +1,13 @@
+package com.fourcode.tracking
+
+import android.app.Application
+import timber.log.Timber
+
+@Suppress("unused")
+class TimberApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Plant a Debug Tree for logging
+        Timber.plant(Timber.DebugTree())
+    }
+}
