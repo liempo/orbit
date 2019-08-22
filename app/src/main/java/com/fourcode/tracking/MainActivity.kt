a6478afd3783ba4c18bae13390968245fdc00305
Dragonborn <gonzalesalec@gmail.com>
2019-08-22 11:54:10 +0800

    Switched to android navigation components

--- app/src/main/java/com/fourcode/tracking/MainActivity.kt
+++ app/src/main/java/com/fourcode/tracking/MainActivity.kt
@@ -2,18 +2,12 @@ package com.fourcode.tracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.fourcode.tracking.ui.auth.AuthFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, AuthFragment.newInstance())
-                .commitNow()
-        }
     }
 
 }
