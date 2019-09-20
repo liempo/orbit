48f0308cb8f661ee2088e0bf6fe69d30f990c38f
Dragonborn <gonzalesalec@gmail.com>
2019-09-20 23:34:47 +0800

    Planted a tree in main activity

--- app/src/main/java/com/fourcode/tracking/MainActivity.kt
+++ app/src/main/java/com/fourcode/tracking/MainActivity.kt
@@ -2,10 +2,14 @@ package com.fourcode.tracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Plant a timber debug tree
+        Timber.plant(Timber.DebugTree())
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
