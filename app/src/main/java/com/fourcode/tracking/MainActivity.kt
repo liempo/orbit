3ba6a0f23293f8b7ea9657e1d38ad04abe84b062
Dragonborn <gonzalesalec@gmail.com>
2019-09-24 01:53:04 +0800

    Initialized Mapbox in activity level

--- app/src/main/java/com/fourcode/tracking/MainActivity.kt
+++ app/src/main/java/com/fourcode/tracking/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fourcode.tracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Plant a timber debug tree
         Timber.plant(Timber.DebugTree())
+
+        // Initialize Mapbox here because it will be called by
+        // multiple fragments under this Activity
+        Mapbox.getInstance(this, BuildConfig.MapboxApiKey)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
